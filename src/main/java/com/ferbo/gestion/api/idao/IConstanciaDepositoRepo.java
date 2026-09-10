e0fa8bf653afc5376290a5a1d083d5cd6d09f38c
calberto <rinoa_heartili@hotmail.com>
2026-09-10 16:49:58 -0600

    Se modifica endpoint del kardex para recibir el folio cliente para su consulta

--- src/main/java/com/ferbo/gestion/api/idao/IConstanciaDepositoRepo.java
+++ src/main/java/com/ferbo/gestion/api/idao/IConstanciaDepositoRepo.java
@@ -1,11 +1,12 @@
 package com.ferbo.gestion.api.idao;
 
-import com.ferbo.gestion.core.commons.dao.GenericDAO;
-import com.ferbo.gestion.core.model.inventario.entrada.ConstanciaDeposito;
 import java.time.LocalDate;
 import java.util.List;
+import com.ferbo.gestion.core.commons.dao.GenericDAO;
+import com.ferbo.gestion.core.model.inventario.entrada.ConstanciaDeposito;
 
 public interface IConstanciaDepositoRepo extends GenericDAO<ConstanciaDeposito, Integer>
 {
     List<ConstanciaDeposito> buscarPorKardex(LocalDate fechaInicio, LocalDate fechaFin, Integer idCliente, Integer idPlanta);
+    ConstanciaDeposito buscarPorFolio(String folio);
 }
